d5ad46eecc33c6723b623b6bc1253c80f991b756
Waqar Ahmed <waqar.17a@gmail.com>
2020-09-06 20:20:32 +0500

    Add example main file

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "utest.h"
+
+#include <string>
+#include <vector>
+
+void test_stringEquality()
+{
+	std::string s = "Hello";
+	UASSERT(s == "Hello");
+}
+
+void test_vector()
+{
+	std::vector<int> v{1,2,3,4};
+	UASSERT(v.at(2) == 3);
+}
+
+void test_failure()
+{
+	int x = 10;
+	UASSERT(x == 9);
+}
+
+int main()
+{
+	UTest t;
+
+	test_stringEquality();
+	test_vector();
+	test_failure();
+
+	return 0;
+}
